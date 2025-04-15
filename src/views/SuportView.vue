af0b0ec5aa8463bac630ce4340374b19083f4cab
nicolefemello <nicfmello@gmail.com>
2025-04-14 21:54:23 -0300

    FEAT: create SuportView.vue #9

---
+++ src/views/SuportView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <section id="banner" class="grid justify-center">
+    <div class="flex justify-center">
+      <h1>Suporte</h1>
+    </div>
+
+    <div class="w-40 h-40 md:w-44 md:h-44 rounded-full object-cover bg-[#D9D9D9]"></div>
+
+    <p>Qual sua dúvida? Envie ela aqui e logo a equipe do E-MOVE entrará em contato</p>
+  </section>
+</template>
